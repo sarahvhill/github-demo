de970361fc23f0344343b005e18dab7bc2ef0ada
Sarah Hill <hill.sarahv@gmail.com>
2026-02-20 09:35:46 -0500

    update

--- example code.R
+++ example code.R
@@ -1,1 +1,8 @@
 # Here is going to be a script where I would write code!
+
+
+# new section of code added -----------------------------------------------
+
+# hello Catherine! - Sarah Feb 20th, 2026 
+
+###############################################
