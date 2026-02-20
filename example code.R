5918362ba6b447f1bca5a9a46da49b28985be86a
Sarah Hill <hill.sarahv@gmail.com>
2026-02-20 10:02:03 -0500

    added plot

--- example code.R
+++ example code.R
@@ -5,4 +5,11 @@
 
 # hello Catherine! - Sarah Feb 20th, 2026 
 
+
+plot(iris$Sepal.Width, iris$Sepal.Length,
+     col = iris$Species,
+     pch = 19,
+     main = "Sepal Width vs Sepal Length",
+     xlab = "Sepal Width", ylab = "Sepal Length")
+
 ###############################################
